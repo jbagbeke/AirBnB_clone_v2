a79ce196c12ba966b3515061d77108595adc9f3b
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-21 20:55:09 +0000

    AirBnB_clone_v2 By Alx Mandatory task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,4 +5,4 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 CREATE DATABASE IF NOT EXISTS performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
