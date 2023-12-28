e037ccfe82ddf4a17070251044627c6fddc64004
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-28 23:55:38 +0000

    0x04. AirBnB clone - Web framework Mandatory task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application
+                                    """
+from flask import Flask
+
+
+# Creating the Flask instance
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_display():
+    """
+        Displays “Hello HBNB!”
+                                """
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_root():
+    """
+        Displays "HBNB"
+                        """
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_C(text):
+    """
+        Displays C followed by input
+                                    """
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+@app.route('/python/<text>', defaults={'txt': 'is cool'}, strict_slashes=False)
+def display_Python(text, txt):
+    """
+        Displays Python followed by input
+                                        """
+    return ("Python {}".format(text.replace('_', ' ') if text else txt))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
